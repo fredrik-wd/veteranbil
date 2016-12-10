2c1e1e2aa0cd4dbc30de33ccc5aaf1a685c8b81f
Fredrik F <ads.jriewwfrt@icloud.com>
2016-12-10 18:12:53 +0100

    Update index.php

--- index.php
+++ index.php
@@ -390,10 +390,10 @@
                     <br>
                     <?php
                     
-                    $servername = "fwinvest.eu.mysql";
-                    $username = "fwinvest_eu_bilbutikk";
+                    $servername = "SKJULT AV SIKKERHETSHENSYN";
+                    $username = "SKJULT AV SIKKERHETSHENSYN";
                     $password = "SKJULT AV SIKKERHETSHENSYN";
-                    $dbname = "fwinvest_eu_bilbutikk";
+                    $dbname = "SKJULT AV SIKKERHETSHENSYN";
                     
                     $conn = new mysqli($servername, $username, $password, $dbname);
 
